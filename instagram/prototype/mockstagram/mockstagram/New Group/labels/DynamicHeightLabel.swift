7a9b7b82a11a305c3f994e62d5a7fcf5650deef0
Brad Phillips <brad@bradphillips.org>
2018-10-15 11:47:56 -0500

    Continue Instagram profile page updates.

--- instagram/prototype/mockstagram/mockstagram/New Group/labels/DynamicHeightLabel.swift
+++ instagram/prototype/mockstagram/mockstagram/New Group/labels/DynamicHeightLabel.swift
@@ -14,8 +14,7 @@ class DynamicHeightLabel: UIView
     var containerView : UIView = UIView(frame: .zero)
     var label : UILabel = UILabel()
     var labelFont = UIFont(name: theme.fontNamePrimaryRegular, size: 14)!
-    var labelHeight : CGFloat = 25
-    var labelHeightConst = NSLayoutConstraint()
+    var variableHeightConst = NSLayoutConstraint()
     
     /** Overrides **/
     override init(frame: CGRect)
@@ -34,89 +33,52 @@ class DynamicHeightLabel: UIView
     {
         self.init(frame: .zero)
         self.label.text = withText
-        self.label.font = labelFont
+    }
+    
+    convenience init(withText: String, andFont: UIFont)
+    {
+        self.init(frame: .zero)
+        self.label.text = withText
+        self.labelFont = andFont
+        self.label.font = self.labelFont
     }
     
     /** Custom methods **/
     func commonInit()
     {
-        containerView.backgroundColor = .red
+        self.backgroundColor = theme.bgColorPrimary
+        
         self.addSubview(containerView)
         containerView.translatesAutoresizingMaskIntoConstraints = false
-        self.addConstraints(getContainerConstraints(forSuperview: self))
-        
-        label.backgroundColor = .yellow
-        label.numberOfLines = 0
-        containerView.addSubview(label)
-        label.translatesAutoresizingMaskIntoConstraints = false
-        containerView.addConstraints(
-            getLabelConstraints(forSuperview: containerView)
+        self.addConstraints(
+            getContainerViewConstraints(toView: containerView, forView: self)
         )
         
+        label.numberOfLines = 0
+        label.font = labelFont
+        containerView.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        var labelConstraints : [NSLayoutConstraint] =
+            getFullSizeViewConstraints(toView: containerView, forView: label)
+        labelConstraints.append(getLabelConstraints())
+        containerView.addConstraints(labelConstraints)
         
         let heightConst = NSLayoutConstraint(
             item: self, attribute: .height, relatedBy: .equal,
             toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: 10
         )
-        self.labelHeightConst = heightConst
-        self.addConstraint(labelHeightConst)
-    }
-
-    func getContainerConstraints(forSuperview: UIView) -> [NSLayoutConstraint]
-    {
-        let leadingConst = NSLayoutConstraint(
-            item: containerView, attribute: .leading,
-            relatedBy: .equal, toItem: forSuperview, attribute: .leading,
-            multiplier: 1, constant: 0
-        )
-        let trailingConst = NSLayoutConstraint(
-            item: containerView, attribute: .trailing,
-            relatedBy: .equal, toItem: forSuperview, attribute: .trailing,
-            multiplier: 1, constant: 0
-        )
-        let topConst = NSLayoutConstraint(
-            item: containerView, attribute: .top,
-            relatedBy: .equal, toItem: forSuperview, attribute: .top,
-            multiplier: 1, constant: 0
-        )
-        let heightConst = NSLayoutConstraint(
-            item: containerView, attribute: .height,
-            relatedBy: .greaterThanOrEqual, toItem: nil, attribute: .notAnAttribute,
-            multiplier: 1, constant: labelHeight
-        )
-        
-        return [leadingConst, trailingConst, heightConst, topConst]
+        self.variableHeightConst = heightConst
+        self.addConstraint(variableHeightConst)
     }
     
-    func getLabelConstraints(forSuperview: UIView) -> [NSLayoutConstraint]
+    func getLabelConstraints() -> NSLayoutConstraint
     {
-        let leadingConst = NSLayoutConstraint(
-            item: label, attribute: .leading,
-            relatedBy: .equal, toItem: forSuperview, attribute: .leading,
-            multiplier: 1, constant: theme.primaryStackSpacingX
-        )
-        let topConst = NSLayoutConstraint(
-            item: label, attribute: .top,
-            relatedBy: .equal, toItem: forSuperview, attribute: .top,
-            multiplier: 1, constant: 0
-        )
-        let botConst = NSLayoutConstraint(
-            item: label, attribute: .bottom,
-            relatedBy: .equal, toItem: forSuperview, attribute: .bottom,
-            multiplier: 1, constant: 0
-        )
-        let widthConst = NSLayoutConstraint(
-            item: label, attribute: .width,
-            relatedBy: .equal, toItem: forSuperview, attribute: .width,
-            multiplier: 1, constant: -(theme.primaryStackSpacingX * 2)
-        )
-        widthConst.priority = UILayoutPriority(rawValue: 999)
         let heightConst = NSLayoutConstraint(
             item: label, attribute: .height,
             relatedBy: .greaterThanOrEqual, toItem: nil, attribute: .notAnAttribute,
             multiplier: 1, constant: 0
         )
-        return [leadingConst, topConst, botConst, widthConst, heightConst]
+        return heightConst
     }
     
     func resetLabelHeight(forWidth : CGFloat)
@@ -124,7 +86,7 @@ class DynamicHeightLabel: UIView
         if let newHeight = self.label.text?.height(
             withConstrainedWidth: forWidth, font: self.labelFont)
         {
-            self.labelHeightConst.constant = newHeight
+            self.variableHeightConst.constant = newHeight
         }
     }
 }
