b107da0eb903d74939bd029694f7190e0eb9aa12
Brad Phillips <brad@bradphillips.org>
2018-11-08 23:01:09 -0600

    Start building out post comments flow.

---
+++ instagram/prototype/mockstagram/mockstagram/New Group/objects/Comment.swift
@@ -0,0 +1,9 @@
+//
+//  Comment.swift
+//  mockstagram
+//
+//  Created by Brad Phillips on 11/8/18.
+//  Copyright © 2018 megaBreezy. All rights reserved.
+//
+
+import Foundation
---
+++ instagram/prototype/mockstagram/mockstagram/New Group/views/CommentsListVC.swift
@@ -0,0 +1,30 @@
+//
+//  CommentsListVC.swift
+//  mockstagram
+//
+//  Created by Brad Phillips on 11/8/18.
+//  Copyright © 2018 megaBreezy. All rights reserved.
+//
+
+import UIKit
+
+class CommentsListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
